06de8084646975545e900d94b3e4159ae094d111
OssaBellator <141500316+OssaBellator@users.noreply.github.com>
2026-07-28 22:12:15 +1000

    Add correlated terminal-bin audit driver

---
+++ scripts/check_terminal_normalizer_correlated_bin_envelope.cpp
@@ -0,0 +1,4 @@
+#include "terminal_normalizer_bin_envelope_part1.inc"
+#include "terminal_normalizer_bin_envelope_part2.inc"
+#include "terminal_normalizer_bin_envelope_part3.inc"
+#include "terminal_normalizer_bin_envelope_part4.inc"
